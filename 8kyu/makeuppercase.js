67269c64010535dd214913a80266d42025431afd
Lucia Inglese <152501487+mluciainglese@users.noreply.github.com>
2025-03-08 15:36:51 +0000

    converts the input string to uppercase

---
+++ 8kyu/makeuppercase.js
@@ -0,0 +1,8 @@
+// Write a function which converts the input string to uppercase.
+
+// Solution
+
+function makeUpperCase(str) {
+    let upperCaseStr = str.toUpperCase();
+    return str.toUpperCase()
+  }
