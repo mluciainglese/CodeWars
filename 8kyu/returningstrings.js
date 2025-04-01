83be5e9a13db8d1f3a40b99b3d905dd1f88f69ad
Lucia Inglese <152501487+mluciainglese@users.noreply.github.com>
2025-04-01 17:11:08 +0100

    returns string including parameter given

---
+++ 8kyu/returningstrings.js
@@ -0,0 +1,8 @@
+/* Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+*/
+
+// Solution
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
